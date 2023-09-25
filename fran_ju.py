294bbe6f1f7bec3d226ab003e25f83827e219269
jussara2 <144683878+jussara2@users.noreply.github.com>
2023-09-25 09:12:27 -0300

    soma dos 5 numeros

--- fran_ju.py
+++ fran_ju.py
@@ -4,4 +4,4 @@ n3 =int( input("digite o terceiro numero:"))
 n4 =int( input("digite o quarto numero:"))
 n5 =int( input("digite o quinto numero:"))
 r = n1+n2+n3+n4+n5
-print("o resultado e:",r)
+print("o resultado e:",r) 
