7294edd5033232595353f034b6de9cc339e9c444
jussara2 <144683878+jussara2@users.noreply.github.com>
2023-09-25 08:56:19 -0300

    prova

---
+++ fran_ju.py
@@ -0,0 +1,7 @@
+n1 =int( input("digite o primero numero:"))
+n2 =int( input("digite o segundo numero:"))
+n3 =int( input("digite o terceiro numero:"))
+n4 =int( input("digite o quarto numero:"))
+n5 =int( input("digite o quinto numero:"))
+r = n1+n2+n3+n4+n5
+print("o resultado e:",r)
